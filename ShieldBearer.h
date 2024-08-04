546d18d1e733ff3dbd64ea2683e105f0e15d510e
Tshegofatso Mahlase <mahlasetshegofatso466@gmail.com>
2024-08-04 22:51:07 +0200

    initial modification of the ShieldBearer.h

---
+++ ShieldBearer.h
@@ -0,0 +1,19 @@
+#ifndef SHIELDBEARER_H
+#define SHIELDBEARER_H
+
+#include "Soldiers.h"
+
+class Shieldbearer : public Soldiers
+{
+private:
+    void prepare();
+    void execute();
+    void retreat();
+    void rest();
+
+public:
+    void engage();
+    void disengage();
+};
+
+#endif
