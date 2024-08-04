40d5ab09e5da7f6d028f2b492158d1ab25bd050a
Tshegofatso Mahlase <mahlasetshegofatso466@gmail.com>
2024-08-04 21:55:35 +0200

    Initial commit

---
+++ Soldiers.h
@@ -0,0 +1,16 @@
+#ifndef SOLDIERS_H
+#define SOLDIERS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+
+class Soldiers{
+    private:
+        int healthPerSoldier;
+        int damagePerSoldier;
+};
+
+#endif
